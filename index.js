728a671ddce5957ac53c87ad11266906235fcaee
Ashfatul Islam <59108821+Ashfatul@users.noreply.github.com>
2023-06-02 19:03:00 +0600

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.DB_PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ashfatul-islam.n5mxo6a.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,9 +25,62 @@ async function run() {
          res.send("Project Server Is Running");
       });
 
-      app.post("/projects", (req, res) => {
+      app.post("/projects", async (req, res) => {
          const data = req.body;
-         const result = projects.insertOne(data);
+         const tags = data.tags.split(",");
+         const setData = {
+            title: data.title,
+            photo: data.photo,
+            description: data.description,
+            date: data.date,
+            tags: tags,
+            github: data.github,
+            liveSite: data.liveSite,
+         };
+         const result = await projects.insertOne(setData);
+         res.send(result);
+      });
+
+      app.get("/projects", async (req, res) => {
+         const max = parseInt(req.query.limit);
+         const result = await projects
+            .find()
+            .sort({ date: -1 })
+            .limit(max)
+            .toArray();
+         res.send(result);
+      });
+
+      app.delete("/project/:id", async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: new ObjectId(id) };
+         const result = projects.deleteOne(query);
+         res.send(result);
+      });
+
+      app.get("/update-project/:id", async (req, res) => {
+         const id = req.params.id;
+         const query = { _id: new ObjectId(id) };
+         const result = await projects.findOne(query);
+         res.send(result);
+      });
+      app.put("/update-project/:id", async (req, res) => {
+         const id = req.params.id;
+         const data = req.body;
+         const tags = data.tags.split(",");
+         const setData = {
+            $set: {
+               title: data.title,
+               photo: data.photo,
+               description: data.description,
+               date: data.date,
+               tags: tags,
+               github: data.github,
+               liveSite: data.liveSite,
+            },
+         };
+         const query = { _id: new ObjectId(id) };
+         const result = projects.updateOne(query, setData);
          res.send(result);
       });
    } catch (error) {
